9f58e44e181db3dfe7b9748f0d9cfe1c1b062608
Daniel Buchanan <daniel@getmorepc.com>
2019-02-20 10:59:04 -0500

    first commit
    first

---
+++ CSharpPractice/CSharpPractice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Program
+{
+    public static string IsEvenOrOdd(int num)
+    {
+        num = 15;
+
+        if (num % 2 != 0)
+        {
+            return ("odd");
+        }
+        else return ("even");
+    }
+}
