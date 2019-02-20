aa451a6c6359f6a6f32d2e2097010b278043b06c
Daniel Buchanan <daniel@getmorepc.com>
2019-02-20 11:13:52 -0500

    I am a huge failure
    fail

---
+++ ClassRunner/ClassRunner/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IsEvenOrOdd(int num)
+            {
+                if (num % 2 != 0)
+                {
+                    print("odd");
+						else {
+                        print("even");
+                    }
+                }
+            }
+    }
+}
