583205a94c16971cef9b9f206c5c0534fd348e76
Daniel Buchanan <daniel@getmorepc.com>
2019-02-20 15:12:37 -0500

    Array Sorter
    Kinda proud of this code

---
+++ StringParser/StringParser/FindLargestArrayNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringParser
+{
+    public class Program
+    {
+        //Create a function that takes an array of numbers. Return the largest number in the array.
+
+        public static int FindLargestNum(int[] arr)
+        {
+            int index = 0;
+            int firstNumber;
+            int biggest = 0;
+
+            while (index < arr.Length)
+            {
+                firstNumber = arr[index];
+
+                if (firstNumber > biggest)
+                {
+                    biggest = firstNumber;
+                }
+                index++;
+
+            }
+            return biggest;
+        }
+    }
+}
