7fc984aa6467f26612073d81ef662d9eed3201a6
Daniel Buchanan <daniel@getmorepc.com>
2019-02-20 13:26:36 -0500

    more solved questions, i don't suck as bad anymore

---
+++ ClassLibrary2/ClassLibrary2/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    public class Class1
+    {
+        public static bool TimeForMilkAndCookies(int year, int month, int day)
+        {
+            if (month == 12 && day == 24)
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
---
+++ ClassLibrary3/ClassLibrary3/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary3
+{
+    public class Class1
+    {
+        //Create a function that takes a number as an argument. 
+        //Add up all the numbers from 1 to the number you passed to the function. 
+        //For example, if the input is 4 then your function 
+        //should return 10 because 1 + 2 + 3 + 4 = 10.
+        public static int AddUp(int num)
+        {
+            int y = num;
+
+            for (int i = 0; i < y; i++)
+            {
+                num = num + i;
+            }
+
+            return num;
+        }
+    }
+}
