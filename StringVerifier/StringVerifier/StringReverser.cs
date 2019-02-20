64408a600c2cdcdd92fc1a5943247e61c846616f
Daniel Buchanan <daniel@getmorepc.com>
2019-02-20 17:03:38 -0500

    stumped

---
+++ StringVerifier/StringVerifier/StringReverser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayValues
+{
+    public class StringReverser
+    {
+        //Create a function that takes a string as its argument and returns the string in reversed order.
+
+        public static string Reverse(string str)
+        {
+            char[] backwards = str.ToCharArray();
+            Array.Reverse(backwards);
+            return backwards.ToString();
+        }
+    }
+}
