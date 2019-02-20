5f5b1cffa46025d886403c3c8adefcd5513bbc08
Daniel Buchanan <daniel@getmorepc.com>
2019-02-20 16:06:14 -0500

    arrays are my pet

---
+++ StringVerifier/StringVerifier/ArrayValues.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayValues
+{
+    public class ArrayValuesManipulator
+    {
+        //Create a function that takes an array of numbers and returns the following statistics:
+        //Minimum Value
+        //Maximum Value
+        //Sequence Length
+        //Average Value
+
+        public static double[] MinMaxLengthAverage(double[] arr)
+        {
+            double[] answer = new double[4];
+            double min = arr.Min();
+            double max = arr.Max();
+            double length = arr.Length;
+            double average = arr.Average();
+
+            answer[0] = min;
+            answer[1] = max;
+            answer[2] = length;
+            answer[3] = average;
+
+            return answer;
+        }
+    }
+}
