99eb53cfa0b239828895fdb577aeaf9a166d5615
Daniel Buchanan <daniel@getmorepc.com>
2019-02-21 15:33:44 -0500

    I am a string manipulator

---
+++ ConsoleApp4/ConsoleApp4/SortNumsAscending.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+public class Program
+{
+    //Create a function that takes an array of numbers and returns 
+    //a new array, sorted in ascending order(smallest to biggest)
+    //Sort numbers array in ascending order.
+    //If functions argument is null, an empty array or undefined, return an empty array.
+    //Return new array of sorted numbers.
+
+    public static int[] SortNumsAscending(int[] arr)
+    {
+        //sort the int array
+        Array.Sort<int>(arr);
+        return arr;
+    }
+}
---
+++ ShuffleTheName.cs
@@ -0,0 +1,14 @@
+﻿using System;
+public class Program
+{
+    //Create a function that accepts a string (of a persons first and last name) 
+    //and returns a string with the first and last name swapped.
+
+    public static string NameShuffle(string str)
+    {
+        string[] words = str.Split(' ');
+        Array.Reverse(words);
+        string answer = String.Format("{0} {1}", words[0], words[1]);
+        return answer;
+    }
+}
