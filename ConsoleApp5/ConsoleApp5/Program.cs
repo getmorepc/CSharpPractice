cd83a48017be6ccc3cbcca8b7c1760b4b7e66a86
Daniel Buchanan <daniel@getmorepc.com>
2019-02-21 16:09:48 -0500

    do while

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 0;
+            int total = 0;
+
+            if (index != arr.Length)
+            {
+                total = total + arr[index];
+                index++;
+            }
+            else return total;
+        }
+    }
+}
---
+++ Mean.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Mean(int[] arr)
+            {
+                int index = 0;
+                double total = 0;
+                double answer = 0;
+
+                do
+                {
+                    total = total + arr[index];
+                    index++;
+                } while (index != arr.Length);
+
+                answer = total / arr.Length;
+                //if (index != arr.Length)
+                //{
+                //    total = total + arr[index];
+                //    index++;
+                //}
+                //else answer = total / arr.Length;
+                return answer;
+            }
+            Mean([1, 0, 4, 5, 2, 4, 1, 2, 3, 3, 3]);
+        }
+    }
+}
