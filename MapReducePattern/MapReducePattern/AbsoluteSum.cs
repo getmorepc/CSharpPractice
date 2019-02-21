131338e06b8b3c095aa879de8b7a393ae98c74bf
Daniel Buchanan <daniel@getmorepc.com>
2019-02-21 16:36:07 -0500

    I am a genius

---
+++ MapReducePattern/MapReducePattern/AbsoluteSum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapReducePattern
+{
+    public class Class1
+    {
+        //Take an array of integers (positive or negative or both) 
+        //and return the sum of the absolute value of each element.
+
+        public static int GetAbsSum(int[] arr)
+        {
+            int index = 0;
+            int total = 0;
+
+            do
+            {
+                arr[index] = Math.Abs(arr[index]);
+                total = total + arr[index];
+                index++;
+            } while (index != arr.Length);
+            return total;
+        }
+    }
+}
