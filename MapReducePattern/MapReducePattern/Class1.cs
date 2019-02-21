880a5f45a1fe741b82bcc0fe8eb870c332496c8f
Daniel Buchanan <daniel@getmorepc.com>
2019-02-21 16:31:11 -0500

    Magnitude
    I am an IT GOD

---
+++ MapReducePattern/MapReducePattern/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapReducePattern
+{
+    public class Class1
+    {
+        //You will be implementing a basic case of the map-reduce pattern in programming. 
+        //Given a vector stored as an array of numbers, find the magnitude of the vector. 
+        //Use the standard distance formula for n-dimensional Cartesian coordinates.
+
+        public static double Magnitude(int[] arr)
+        {
+            int index = 0;
+            int total = 0;
+            double answer = 0;
+
+            if (arr.Length == 0)
+            {
+                return 0;
+            }
+
+            do
+            {
+                total = total + (arr[index] * arr[index]);
+                index++;
+            } while (index != arr.Length);
+            answer = Math.Sqrt(total);
+            return answer;
+        }
+    }
+}
