ca3e2f343f56f10b9d00812dd86447073549827c
Daniel Buchanan <daniel@getmorepc.com>
2019-02-21 16:58:49 -0500

    Easier and Easier

---
+++ SmallestNumberInArray/SmallestNumberInArray/FindSmallestNum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallestNumberInArray
+{
+    public class Class1
+    {
+        //Create a function that takes an array of numbers 
+        //and returns the smallest number in the set.
+
+        public static double FindSmallestNum(double[] arr)
+        {
+            return arr.Min();
+        }
+    }
+}
