e877ae5ec433144203e340298a07f1c303acad7e
Daniel Buchanan <daniel@getmorepc.com>
2019-02-22 10:18:37 -0500

    more arrays?

---
+++ ReturnFourLetterStrings/ReturnFourLetterStrings/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReturnFourLetterStrings
+{
+    public class Class1
+    {
+        //Create a function that takes an array of strings. 
+        //Return all words in the array that are exactly four letters.
+
+        public static string[] IsFourLetters(string[] arr)
+        {
+            return arr.Where(str => str.Length == 4).ToArray();
+        }
+    }
+}
---
+++ SmallestBiggestNumbersFromArray/SmallestBiggestNumbersFromArray/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallestBiggestNumbersFromArray
+{
+    public class Class1
+    {
+        //Create a function that takes an array of numbers and 
+        //return both the minimum and maximum numbers, in that order.
+
+        public static double[] FindMinMax(double[] values)
+        {
+            double[] answer = new double[2];
+
+            answer[0] = values.Min();
+            answer[1] = values.Max();
+
+            return answer;
+        }
+
+    }
+}
